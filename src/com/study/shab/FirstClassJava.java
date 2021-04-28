10130744d2730010fb49c46ddc23ca7620cf7838
SANJUM12 <sanjum@3gsllc.com>
2021-04-28 10:43:47 -0400

    initialcommit

---
+++ src/com/study/shab/FirstClassJava.java
@@ -0,0 +1,21 @@
+package com.study.shab;
+
+public class FirstClassJava {
+    public static int i=5;
+    public int j=10;
+    public static void main(String[] args)
+    {
+        System.out.println(i);
+        //System.out.println
+       // FirstClassJava obj=new FirstClassJava();
+        //System.out.println(obj.j);
+       // obj.j=20;
+       // obj.printi();
+    }
+    public void printi()
+    {
+        System.out.println(i);
+        //System.out.println(j);
+    }
+
+}
